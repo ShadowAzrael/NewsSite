58163bfcf0e0766fd1f8657977cbbdd9259e79a2
ShadowAzrael <909456873@qq.com>
2022-12-21 18:15:52 +0800

    添加项目文件。

---
+++ NewsSite/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsSite.Models;
+using NewsSite.Models.Database;
+using System;
+using System.Linq;
+
+namespace NewsSite.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly NewsSiteContext _db;
+        public TestController(NewsSiteContext db)
+        {
+            _db = db;
+        }
+
+        [Route("/")]
+        [Route("/Index.html")]
+        public IActionResult Index(string kind, string search)
+        {
+            if (search != null)
+            {
+                ViewData["news"] = _db.News.Where(x => x.Title.Contains(search)).ToList();
+            }
+            else if (kind == null || kind == "全部")
+            {
+                ViewData["news"] = _db.News.ToList();
+            }
+            else
+            {
+                ViewData["news"] = _db.News.Where(x => x.Kind == kind).ToList();
+            }
+            return View();
+        }
+
+        [Route("/detail.html")]
+        public IActionResult detail(int id)
+        {
+            ViewData["news"] = _db.News.FirstOrDefault(x => x.Id == id);
+            return View();
+        }
+
+    }
+}
---
+++ NewsSite/Models/Database/News.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NewsSite.Models.Database
+{
+    public partial class News
+    {
+        public int Id { get; set; }
+        public string Kind { get; set; }
+        public string Title { get; set; }
+        public string NewsContent { get; set; }
+        public string Photo { get; set; }
+        public string Source { get; set; }
+        public DateTime? Time { get; set; }
+    }
+}

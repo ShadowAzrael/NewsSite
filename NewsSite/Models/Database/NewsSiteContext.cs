b237e4071471c3d3f36be0f949e97b183ceefa47
ShadowAzrael <909456873@qq.com>
2022-12-21 18:15:52 +0800

    finished

---
+++ NewsSite/Models/Database/NewsSiteContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace NewsSite.Models.Database
+{
+    public partial class NewsSiteContext : DbContext
+    {
+        public NewsSiteContext()
+        {
+        }
+
+        public NewsSiteContext(DbContextOptions<NewsSiteContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<News> News { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=NewsSite;Integrated Security=SSPI");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Chinese_PRC_CI_AS");
+
+            modelBuilder.Entity<News>(entity =>
+            {
+                entity.ToTable("news");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Kind)
+                    .HasMaxLength(50)
+                    .HasColumnName("kind");
+
+                entity.Property(e => e.NewsContent)
+                    .IsRequired()
+                    .HasMaxLength(2000)
+                    .HasColumnName("news_content");
+
+                entity.Property(e => e.Photo)
+                    .HasMaxLength(200)
+                    .HasColumnName("photo");
+
+                entity.Property(e => e.Source)
+                    .HasMaxLength(50)
+                    .HasColumnName("source");
+
+                entity.Property(e => e.Time)
+                    .HasColumnType("date")
+                    .HasColumnName("time");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("title");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ NewsSite/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NewsSite.Models;
+using NewsSite.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsSite
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();//新增该行代码，为程序注入MVC服务
+
+            services.AddDbContext<NewsSiteContext>();//注入DbContext
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //使用路由
+            app.UseRouting();
+
+            //使用静态文件
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
